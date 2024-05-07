c76854768e67c66f497b39fcbe34c97902f7d4c6
hoangvu-dot <mhoang.vuduc@gmail.com>
2024-05-08 00:37:55 +0700

    Formatting

--- Database.py
+++ Database.py
@@ -1,38 +1,44 @@
 import mysql.connector
 from SAVING import take_value
 
+
 def create_database(mycursor):
-  try:
-      mycursor.execute("CREATE TABLE Trackers (Date VARCHAR(255), Payment VARCHAR(50), Cost int UNSIGNED, Product VARCHAR(255), id int PRIMARY KEY AUTO_INCREMENT)")
-  except:
-      mycursor.execute("INSERT INTO Trackers (Date, Payment, Cost, Product) VALUES (%s, %s, %s, %s)", take_value())
-  else:
-      mycursor.execute("INSERT INTO Trackers (Date, Payment, Cost, Product) VALUES (%s, %s, %s, %s)", take_value())
-      
+    try:
+        mycursor.execute(
+            "CREATE TABLE Trackers (Date VARCHAR(255), Payment VARCHAR(50), Cost int UNSIGNED, Product VARCHAR(255), id int PRIMARY KEY AUTO_INCREMENT)"
+        )
+    except:
+        mycursor.execute(
+            "INSERT INTO Trackers (Date, Payment, Cost, Product) VALUES (%s, %s, %s, %s)",
+            take_value(),
+        )
+    else:
+        mycursor.execute(
+            "INSERT INTO Trackers (Date, Payment, Cost, Product) VALUES (%s, %s, %s, %s)",
+            take_value(),
+        )
+
+
 def delete_data(mycursor):
-   mycursor.execute(f"DELETE FROM Trackers WHERE id ORDER BY id DESC LIMIT 1")
+    mycursor.execute(f"DELETE FROM Trackers WHERE id ORDER BY id DESC LIMIT 1")
 
 
 def main():
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database = "project_database"
-  )
+    mydb = mysql.connector.connect(
+        host="localhost", user="root", password="root", database="project_database"
+    )
 
-  input = "0"
-  mycursor = mydb.cursor()
+    input = "0"
+    mycursor = mydb.cursor()
 
-  # 1 for Create & 0 for delete
+    # 1 for Create & 0 for delete
 
-  mydb.commit()
+    mydb.commit()
 
-  mycursor.execute("SELECT * from Trackers")
- 
-  for x in mycursor:
-     print(x)
- 
+    mycursor.execute("SELECT * from Trackers")
+
+    for x in mycursor:
+        print(x)
+
+
 main()
-
-
--- UI.py
+++ UI.py
@@ -7,16 +7,31 @@ from SAVING import take_value
 alt.themes.enable("dark")
 
 with st.sidebar:
-    
-    st.title('💵 Budgeting Tracker')
+    st.title("💵 Budgeting Tracker")
 
-    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
-    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
-    
+    color_theme_list = [
+        "blues",
+        "cividis",
+        "greens",
+        "inferno",
+        "magma",
+        "plasma",
+        "reds",
+        "rainbow",
+        "turbo",
+        "viridis",
+    ]
+    selected_color_theme = st.selectbox("Select a color theme", color_theme_list)
+
     box_date = st.text_input("What's the date today: ")
     box_payment = st.text_input("What's the payment method: ")
-    box_cost = st.number_input("How much does it cost: ", value=None, placeholder="Type a number...")
+    box_cost = st.number_input(
+        "How much does it cost: ", value=None, placeholder="Type a number..."
+    )
     box_product = st.text_input("What did you spend on: ")
 
-    st.button("Another Spending day 🥲", on_click= take_value, args=(box_date,box_payment,box_cost,box_product))
-
+    st.button(
+        "Another Spending day 🥲",
+        on_click=take_value,
+        args=(box_date, box_payment, box_cost, box_product),
+    )
