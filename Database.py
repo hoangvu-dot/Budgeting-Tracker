12e12b8a1d468e455a4aec3f1af21938461389a2
hoangvu-dot <mhoang.vuduc@gmail.com>
2024-05-10 08:39:28 +0700

    Fourth day
    With additional function, I have create the total amount of money spent in a month, also creating a history dataframe, and a unique item function in database file. Which later  will be used for a bar chart to display unique item purchased

--- Database.py
+++ Database.py
@@ -1,11 +1,11 @@
 import mysql.connector
 from SAVING import take_value
+import numpy as np
 
-
-def create_database(date, payment, cost, product):
+def create_databases(date, payment, cost, product):
     try:
         mycursor.execute(
-            "CREATE TABLE Trackers (Date VARCHAR(255), Payment VARCHAR(50), Cost int UNSIGNED, Product VARCHAR(255), id int PRIMARY KEY AUTO_INCREMENT)"
+            "CREATE TABLE Trackers (Date VARCHAR(255), Payment VARCHAR(50), Cost int UNSIGNED, Product VARCHAR(255))"
         )
     except:
         mycursor.execute(
@@ -13,29 +13,63 @@ def create_database(date, payment, cost, product):
             take_value(date, payment, cost, product),
         )
         mydb.commit()
-    else:
-        mycursor.execute(
-            "INSERT INTO Trackers (Date, Payment, Cost, Product) VALUES (%s, %s, %s, %s)",
-            take_value(date, payment, cost, product),
-        )
-        mydb.commit()
         
-
 def show_data():
     mycursor.execute("SELECT * from Trackers")
     for x in mycursor:
         print(x)
-    
+        
+def retrieve_data():
+    mycursor.execute("SELECT * from Trackers")
+    lst = [x for x in mycursor]
+    return lst
 
 def delete_data():
     mycursor.execute(f"DELETE FROM Trackers WHERE id ORDER BY id DESC LIMIT 1")
     mydb.commit()
 
+def monthly_spend() -> int:
+    mycursor.execute("SELECT Cost from Trackers")
+    total = 0
+    for x in mycursor:
+        total += x[0]
+    return total
+
+def unique_item():
+    mycursor.execute("SELECT Product from Trackers")
+    item = {}
+    for x in mycursor:
+        if x not in item:
+            item[x] = 1
+        else:
+            item[x] += 1
+    
+    number_transaction = len(retrieve_data())
+    for x in item:
+        item[x] /= number_transaction
+        item[x] *= 100
+
+    return item
+
 
 mydb = mysql.connector.connect(
-    host="localhost", user="root", password="root", database="project_database"
+    host="localhost", 
+    user="root", 
+    password="root", 
+    database="project_database"
 )
 mycursor = mydb.cursor()
 
+"""
+mycursor.execute("TRUNCATE Trackers")
+mydb.commit()
+"""
+
+def main():
+    #create_databases("9/5/2024", "Cash", 100.0, "Food")
+    print(monthly_spend())
+
+if __name__ == "__main__":
+    main()
 
 
--- UI.py
+++ UI.py
@@ -3,25 +3,72 @@ import pandas as pd
 import altair as alt
 import plotly.express as px
 from Database import *
+from streamlit_extras.metric_cards import style_metric_cards
 
-
+st.set_page_config(layout="wide")
 alt.themes.enable("dark")
 
 with st.sidebar:
     st.title("💵 Budgeting Tracker")
 
-
     box_date = st.text_input("What's the date today: ")
     box_payment = st.text_input("What's the payment method: ")
     box_cost = st.number_input(
-        "How much does it cost: ", value=None, placeholder="Type a number..."
+        "How much does it cost: ", value = None ,placeholder="Type a number..."
     )
     box_product = st.text_input("What did you spend on: ")
 
-    st.button(
-        "Another Spending day 🥲",
-        on_click=create_database,
-        args=(box_date, box_payment, box_cost, box_product)
-    )
-    
+    col = st.columns([3,1], gap = "small")
+    with col[0]:
+        st.button(
+            "Another Spending day 🥲",
+            on_click=create_databases,
+            args= (box_date, box_payment, box_cost, box_product)             
+        )
+    with col[1]:
+        st.button(
+            "Undo",
+            on_click= delete_data,
+        )
+
+col1, col2 = st.columns(2, gap= "large")
+with col1:
+   
+    with st.container(height=200):
+        
+        st.subheader(":orange[Current Month Damage]")
+        st.metric(label="Idontknow",value =f'{monthly_spend()}.000 VND', label_visibility= "hidden")
+        color = 0
+        if monthly_spend() > 3000:
+            color = '#E74C3C'
+        else:
+            color = "#27AE60"
+        style_metric_cards(background_color= color,border_left_color=color,border_color=color)
+
+    with st.container(height=300):
+        st.markdown(":orange[**Transactions**]")
+        df = pd.DataFrame(
+            {
+                "date": [x[0] for x in retrieve_data()],
+                "payment": [x[1] for x in retrieve_data()],
+                "cost": [x[2] for x in retrieve_data()],
+                "reason": [x[3] for x in retrieve_data()],
+            }
+        )
+        df_reversed = df[::-1]
+        st.dataframe(
+            df_reversed,
+            column_config={
+                "date": "Date",
+                "payment": "Payment",
+                "cost": "Cost",
+                "reason": "What u spend on",
+            },
+            hide_index=True, use_container_width= True, 
+        )
+
+with col2:
+   ...
+   
+
 
